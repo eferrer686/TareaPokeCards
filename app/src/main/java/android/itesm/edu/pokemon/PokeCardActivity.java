036b3a2a6585799345489db12d9b672ffdba8a05
Eduardo Ferrer Mac Gregor Ruiz <edfmgr@gmail.com>
2019-02-26 12:43:45 -0600

    App

---
+++ app/src/main/java/android/itesm/edu/pokemon/PokeCardActivity.java
@@ -0,0 +1,40 @@
+package android.itesm.edu.pokemon;
+
+import android.itesm.edu.pokemon.model.PokeCard;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+
+public class PokeCardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_poke_card);
+
+        PokeCard pokeCard = (PokeCard) getIntent().getSerializableExtra("pokemon");
+        loadPokemon(pokeCard);
+
+    }
+
+    private void loadPokemon(PokeCard pokeCard){
+        TextView id,name,artist;
+        ImageView image;
+        id = findViewById(R.id.id_card);
+        name = findViewById(R.id.card_name);
+        artist = findViewById(R.id.author_card);
+        image = findViewById(R.id.card);
+
+        RequestOptions options = new RequestOptions().centerCrop().placeholder(R.drawable.load_card).error(R.drawable.load_card);
+
+        name.setText(pokeCard.getName());
+        id.setText(pokeCard.getId());
+        artist.setText(pokeCard.getArtist());
+
+        Glide.with(this).load(pokeCard.getImageUrl()).apply(options).into(image);
+    }
+}
---
+++ app/src/main/java/android/itesm/edu/pokemon/adapters/PokemonRecyclerAdapter.java
@@ -0,0 +1,89 @@
+package android.itesm.edu.pokemon.adapters;
+
+import android.app.DownloadManager;
+import android.content.Context;
+import android.content.Intent;
+import android.itesm.edu.pokemon.PokeCardActivity;
+import android.itesm.edu.pokemon.R;
+import android.itesm.edu.pokemon.model.PokeCard;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.RecyclerView.ViewHolder;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+
+import java.io.Serializable;
+import java.util.List;
+
+public class PokemonRecyclerAdapter extends RecyclerView.Adapter<PokemonRecyclerAdapter.PokeRecordHolder>{
+
+    private Context context;
+    private List<PokeCard> cards;
+    RequestOptions options;
+
+    public PokemonRecyclerAdapter(Context context, List<PokeCard> cards) {
+        this.context = context;
+        this.cards = cards;
+
+        options = new RequestOptions().centerCrop().placeholder(R.drawable.load_card).error(R.drawable.load_card);
+    }
+
+    @NonNull
+    @Override
+    public PokeRecordHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View view;
+        LayoutInflater inflater = LayoutInflater.from(context);
+        view = inflater.inflate(R.layout.poke_card_item,viewGroup,false);
+
+        final PokeRecordHolder pokeRecordHolder = new PokeRecordHolder(view);
+
+        pokeRecordHolder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PokeCard pokeCard = cards.get(pokeRecordHolder.getAdapterPosition());
+                Toast.makeText(context,pokeCard.getName(), Toast.LENGTH_SHORT).show();
+                Intent it = new Intent(context,PokeCardActivity.class);
+                it.putExtra("pokemon", pokeCard);
+
+                context.startActivity(it);
+            }
+        });
+
+        return pokeRecordHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull PokeRecordHolder pokeRecordHolder, int i) {
+        pokeRecordHolder.name.setText(cards.get(i).getName());
+        pokeRecordHolder.id.setText(cards.get(i).getId());
+        pokeRecordHolder.artist.setText(cards.get(i).getArtist());
+
+        Glide.with(context).load(cards.get(i).getImageUrl()).apply(options).into(pokeRecordHolder.image);
+    }
+
+    @Override
+    public int getItemCount() {
+        return cards.size();
+    }
+
+    public static class PokeRecordHolder extends RecyclerView.ViewHolder{
+
+        TextView id,name,artist;
+        ImageView image;
+        public PokeRecordHolder(@NonNull View itemView) {
+            super(itemView);
+            id = itemView.findViewById(R.id.id_card);
+            name = itemView.findViewById(R.id.card_name);
+            artist = itemView.findViewById(R.id.author_card);
+            image = itemView.findViewById(R.id.card);
+        }
+    }
+
+}
